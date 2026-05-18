09d0c20d530b49c9ff68c4d11b965fb766ba958d
frto027 <q602706150@gmail.com>
2026-05-18 13:10:10 +0800

    add multibuild scripts

---
+++ scripts/multibuild/Readme.md
@@ -0,0 +1,3 @@
+# Build for old version
+
+This is some build script for old game versions.
---
+++ scripts/multibuild/build_all.bat
@@ -0,0 +1,3 @@
+python multibuild\apply.py 1.40.6 && qpm restore && qpm s build && qpm qmod zip
+python multibuild\apply.py 1.40.4 && qpm restore && qpm s build && qpm qmod zip
+python multibuild\apply.py 1.37.0 && qpm restore && qpm s build && qpm qmod zip
