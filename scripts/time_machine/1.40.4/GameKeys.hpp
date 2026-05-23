c6e5073e2ce5a2fe9801bbd15b4d6c021f5a33fe
frto027 <q602706150@gmail.com>
2026-05-23 15:55:44 +0800

    add game keys placeholder

---
+++ scripts/time_machine/1.40.4/GameKeys.hpp
@@ -0,0 +1,12 @@
+#pragma once
+// clang-format off
+
+namespace SSL10n {
+    namespace GameKeys {
+        /* N/A */
+        /*
+            The game keys is not generated for this game version.
+            You need manually reference the game keys.
+        */
+    }
+}
